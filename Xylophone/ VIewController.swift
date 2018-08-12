a8cefdab27f8ff7ba5318b8dbbc2ad625967802e
nicolehli <nicolehcli@gmail.com>
2018-08-11 20:59:00 -0700

    Each key plays note1.wav is working

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,23 +7,23 @@
 //
 
 import UIKit
+import AudioToolbox
 
 class ViewController: UIViewController{
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
-
     @IBAction func notePressed(_ sender: UIButton) {
-        
-        
-        
+        // Load "note1.wav"
+        if let soundURL = Bundle.main.url(forResource: "note1", withExtension: "wav") {
+            var mySound: SystemSoundID = 0
+            AudioServicesCreateSystemSoundID(soundURL as CFURL, &mySound)
+            // Play
+            AudioServicesPlaySystemSound(mySound);
+        }
     }
-    
-  
 
 }
 
