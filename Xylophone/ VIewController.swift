d6150cb4457d0f11c36c6cdc2f38783657523c4d
nicolehli <nicolehcli@gmail.com>
2018-08-12 17:56:02 -0700

    Different notes play diff wav files

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,23 +7,40 @@
 //
 
 import UIKit
-import AudioToolbox
+import AVFoundation
 
-class ViewController: UIViewController{
+class ViewController: UIViewController, AVAudioPlayerDelegate{
+    
+    var audioPlayer : AVAudioPlayer!
+    var selectedSoundFileName : String = ""
+    
+        let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func notePressed(_ sender: UIButton) {
-        // Load "note1.wav"
-        if let soundURL = Bundle.main.url(forResource: "note1", withExtension: "wav") {
-            var mySound: SystemSoundID = 0
-            AudioServicesCreateSystemSoundID(soundURL as CFURL, &mySound)
-            // Play
-            AudioServicesPlaySystemSound(mySound);
-        }
+        selectedSoundFileName = soundArray[sender.tag - 1]
+        
+        // print(selectedSoundFileName)
+        playSound()
     }
+    
+    func playSound(){
+        
+        let soundURL = Bundle.main.url(forResource: selectedSoundFileName, withExtension: "wav")
+        
+        do {
+            try audioPlayer = AVAudioPlayer(contentsOf: soundURL!)
+        } catch {
+            print(error)
+            print("Encountered an error")
+        }
+        
+        audioPlayer.play()
 
+    }
+    
 }
 
