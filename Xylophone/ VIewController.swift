521196f572610ed45d82c559a1602d2759853d8d
nicolehli <nicolehcli@gmail.com>
2018-08-12 17:59:27 -0700

    Refactored playSound function

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -12,24 +12,20 @@ import AVFoundation
 class ViewController: UIViewController, AVAudioPlayerDelegate{
     
     var audioPlayer : AVAudioPlayer!
-    var selectedSoundFileName : String = ""
-    
-        let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
+    let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func notePressed(_ sender: UIButton) {
-        selectedSoundFileName = soundArray[sender.tag - 1]
         
-        // print(selectedSoundFileName)
-        playSound()
+        playSound(soundFileName: soundArray[sender.tag - 1])
     }
     
-    func playSound(){
+    func playSound(soundFileName: String){
         
-        let soundURL = Bundle.main.url(forResource: selectedSoundFileName, withExtension: "wav")
+        let soundURL = Bundle.main.url(forResource: soundFileName, withExtension: "wav")
         
         do {
             try audioPlayer = AVAudioPlayer(contentsOf: soundURL!)
